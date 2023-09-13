7070cf671e83f54162ee29ac4910e5a587c4058f
Kei Hibino <ex8k.hibino@gmail.com>
2023-09-13 21:03:10 +0900

    property testing of lookupLE and lookupGT

--- tests/Data/OrdPSQ/Tests.hs
+++ tests/Data/OrdPSQ/Tests.hs
@@ -2,7 +2,11 @@ module Data.OrdPSQ.Tests
     ( tests
     ) where
 
+import           Control.Monad         (replicateM)
 import           Data.List             (isInfixOf)
+import           Data.Maybe
+import           Prelude               hiding (null)
+import           Test.QuickCheck
 import           Test.HUnit            (Assertion, assert)
 import           Test.Tasty            (TestTree)
 import           Test.Tasty.HUnit      (testCase)
@@ -23,6 +27,8 @@ tests =
     , testCase     "invalidLTree"  test_invalidLTree
     , testCase     "balanceErrors" test_balanceErrors
     , testProperty "toAscList"     prop_toAscList
+    , testProperty "lookupLE"      prop_lookupLE
+    , testProperty "lookupGT"      prop_lookupGT
     ]
 
 
@@ -87,3 +93,121 @@ prop_toAscList t = isUniqueSorted [k | (k, _, _) <- toAscList t]
     isUniqueSorted (x : y : zs) = x < y && isUniqueSorted (y : zs)
     isUniqueSorted [_]          = True
     isUniqueSorted []           = True
+
+---
+
+rangeElems :: Gen [Int] -> Int -> (Int, Int) -> Gen [Int]
+rangeElems absurd gsize (n, x)
+    | n > x                            = absurd
+    | toI x - toI n < toI (gsize + 2)  = return [n..x]
+    | otherwise                        = do
+          es <- replicateM gsize $ choose (n + 1, x - 1)
+          return $ n : es ++ [x]
+  where
+    toI i = fromIntegral i :: Integer
+
+type Range k = (k, k)
+type WithLookup k p v a = String -> (k -> OrdPSQ k p v -> Maybe (k, p, v)) -> a
+type LGRanges k = ([(Range k, k)], Range k)
+
+onFail :: Testable prop => (a -> String) -> (a -> prop) -> a -> Property
+onFail em p x = whenFail' (putStrLn $ em x) (p x)
+
+lookupLG_expectJust'
+    :: (Show k, Eq k, Show p, Show v)
+    => WithLookup k p v (OrdPSQ k p v -> ([k], k) -> Property)
+lookupLG_expectJust' name lookup' psq (qks, ek) =
+    conjoin [ propJust lk | lk <- lookups ]
+    .&&.
+    conjoin [ p | (Just p, _qk) <- lookups ]
+  where
+    lookups = [ (match qk, qk) | qk <- qks ]
+
+    propJust = onFail failNothing (isJust . fst)
+    failNothing (_, qk) = "expect Just result of " ++ name ++ ": Nothing result for range: " ++
+                          show (qks, ek) ++ " key: " ++ show qk
+    match qk = (\(rk, _, _) -> onFail failUnmatch (== ek) rk) <$> lookup' qk psq
+      where
+        failUnmatch rk = "expect match result of " ++ name ++ ": unmatch result for case: " ++ show (qks, ek) ++
+                         " key: " ++ show qk ++ " result:" ++ show rk ++ " =/= " ++ "expect:" ++ show ek
+
+lookupLG_expectJust
+    :: (Show p, Show v)
+    => WithLookup Int p v (OrdPSQ Int p v -> (Range Int, Int) -> Property)
+lookupLG_expectJust name lookup' psq (jrange, expect) =
+    {- ioProperty just to generate -}
+    ioProperty $ (\es -> lookupLG_expectJust' name lookup' psq (es, expect)) <$> generate genElems
+  where
+    gsize = 2
+    genElems = rangeElems assertion gsize jrange
+    assertion = error $ "lookupLG_expectJust: inverted range: " ++ show jrange
+
+lookupLG_expectNothing'
+    :: (Show k, Eq k, Show p, Show v)
+    => WithLookup k p v (OrdPSQ k p v -> [k] -> Property)
+lookupLG_expectNothing' name lookup' psq qks =
+    conjoin [ propNothing qk | qk <- qks ]
+  where
+    propNothing = onFail failJust (\qk -> isNothing $ lookup' qk psq)
+    failJust qk =  "expect Nothing result of " ++ name ++ ": Just result for case: " ++
+                   show qks ++ " key: " ++ show qk
+
+lookupLG_expectNothing
+    :: (Show p, Show v)
+    => WithLookup Int p v (OrdPSQ Int p v -> Range Int -> Property)
+lookupLG_expectNothing name lookup' psq nrange =
+    {- ioProperty just to generate -}
+    ioProperty $ lookupLG_expectNothing' name lookup' psq <$> generate genElems
+  where
+    gsize = 2
+    genElems = rangeElems assertion gsize nrange
+    assertion = error $ "lookupLG_expectNothing: inverted range: " ++ show nrange
+
+lookupLG_expects
+    :: (Show p, Show v)
+    => WithLookup Int p v ((OrdPSQ Int p v -> ([(Range Int, Int)], Range Int)) -> OrdPSQ Int p v -> Property)
+lookupLG_expects name lookup' getRanges psq =
+    conjoin [exJust j | j <- jranges ]
+    .&&.
+    exNothing nrange
+  where
+    exJust    = lookupLG_expectJust    name lookup' psq
+    exNothing = lookupLG_expectNothing name lookup' psq
+    (jranges, nrange) = getRanges psq
+
+---
+
+keyIntervals :: Bounded k => OrdPSQ k p v -> ((k, k), [(k, k)], (k, k))
+keyIntervals psq = (head closed, init $ tail closed, last closed)
+  where
+    ks = [ k | (k, _, _) <- toAscList psq ]
+    closed = zip (minBound : ks) (ks ++ [maxBound])
+
+nothingsNullPSQ :: Bounded k => OrdPSQ k p v -> ([(Range k, k)], Range k) -> ([(Range k, k)], Range k)
+nothingsNullPSQ psq notNull
+    | null psq  =  ([], (minBound, maxBound))
+    | otherwise =  notNull
+
+---
+
+prop_lookupLE :: OrdPSQ Int Int Char -> Property
+prop_lookupLE = lookupLG_expects "lookupLE" lookupLE rangesLE
+
+rangesLE :: OrdPSQ Int p v -> LGRanges Int
+rangesLE psq = nothingsNullPSQ psq $ ([just cr | cr <- crs] ++ [justEnd maxRange], nothing minRange)
+  where
+    (minRange, crs, maxRange) = keyIntervals psq
+    just    (n, x)  = ((n, x - 1), n)
+    justEnd (n, x)  = ((n, x)    , n)
+    nothing (n, x)  =  (n, x - 1)
+
+prop_lookupGT :: OrdPSQ Int Int Char -> Property
+prop_lookupGT = lookupLG_expects "lookupGT" lookupGT rangesGT
+
+rangesGT :: OrdPSQ Int p v -> LGRanges Int
+rangesGT psq = nothingsNullPSQ psq $ ([justEnd minRange] ++ [just cr | cr <- crs], nothing maxRange)
+  where
+    (minRange, crs, maxRange) = keyIntervals psq
+    just    (n, x)  = ((n, x - 1), x)
+    justEnd         = just
+    nothing (n, x)  =  (n, x)
